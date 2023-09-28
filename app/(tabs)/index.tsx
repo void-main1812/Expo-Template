85780d020b2fc517bfd8e4e58344ecce00f8a580
sourabh_singh <sourabhsinghredmi@gmail.com>
2023-09-28 13:27:11 +0530

    🎉  Project Initialization v1.0.0

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,33 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { MotiText } from "moti";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <>
+      <View className="h-full w-full bg-white flex justify-center items-center">
+        <View className="h-[100px] w-[100px] rounded-lg shadow-2xl shadow-black overflow-hidden mb-[20px]">
+          <Image
+            className="h-full w-full"
+            source={require("../../assets/images/logo.png")}
+          />
+        </View>
+        <MotiText
+          className="text-gray-900 text-2xl font-bold p-4 text-center w-[70%]"
+        >
+          You can start Editing from here
+        </MotiText>
+        <Text className="p-4 bg-gray-100 rounded-md border border-gray-200">
+          "app/(tabs)/index.tsx"
+        </Text>
+        <View className="text-center flex justify-center items-center absolute bottom-[40px]">
+          <Text>This template was made by </Text>
+          <Link href={"https://github.com/void-main1812"}>
+            <Text className="text-xl font-bold underline">Sourabh Singh</Text>
+          </Link>
+        </View>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
